3d1dbfd0be537e170eb07cbb4701322dd79986b7
icaromedeiros <icaro.medeiros@gmail.com>
2013-02-04 16:10:13 -0200

    calling virtuoso with asyncronous client with a simple query

--- src/servers/tornado_server.py
+++ src/servers/tornado_server.py
@@ -1,12 +1,34 @@
+import json
 import tornado
-from tornado.web import RequestHandler
+from tornado import gen
+from tornado.web import RequestHandler, asynchronous
+from tornado.httpclient import AsyncHTTPClient
+from tornado.httputil import url_concat
+from settings import SPARQL_ENDPOINT
+
 
 class RootHandler(RequestHandler):
 
     #SUPPORTED_METHODS = ("GET", "POST")
 
+    @asynchronous
+    @gen.engine
     def get(self):
-        self.write("WORKING")
+        query = "select distinct ?class where {?class a owl:Class} LIMIT 100"
+        url = url_concat(SPARQL_ENDPOINT, {
+            "query": query,
+            "Accept": "application/sparql-results+json",
+            "format": "application/sparql-results+json"
+        })
+
+        AsyncHTTPClient.configure("tornado.curl_httpclient.CurlAsyncHTTPClient")
+        sparql_endpoint = AsyncHTTPClient()
+
+        response = yield gen.Task(sparql_endpoint.fetch, url)
+        result_dict = json.loads(response.body)
+        pretty_json_string = json.dumps(result_dict, sort_keys=True, indent=4, separators=(",", ":"))
+        self.write(pretty_json_string)
+        self.flush()
 
     def post(self):
         pass
--- src/settings.py
+++ src/settings.py
@@ -1,5 +1,1 @@
-SPARQL_ENDPOINT_REALM = "SPARQL"  # Virtuoso-related
-SPARQL_ENDPOINT_AUTH_MODE = "digest"  # 'basic', ''
-SPARQL_ENDPOINT_AUTH = "http://localhost:8890/sparql-auth"
-SPARQL_ENDPOINT_USER = "api-semantica"
-SPARQL_ENDPOINT_PASSWORD = "api-semantica"
+SPARQL_ENDPOINT = "http://localhost:8890/sparql"
