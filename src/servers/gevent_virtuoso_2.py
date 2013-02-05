0c9699ae5e5bd0425c89e7a1d02874d68f52df69
rodsenra <rodsenra@gmail.com>
2013-02-05 17:43:20 -0200

    Clean-up code

--- src/servers/gevent_virtuoso_2.py
+++ src/servers/gevent_virtuoso_2.py
@@ -6,15 +6,10 @@ from gevent.wsgi import WSGIServer
 
 from flask import Flask
 
-from geventhttpclient import HTTPClient
 from geventhttpclient.url import URL
-
 from geventhttpclient import httplib
 httplib.patch()
 from httplib2 import Http
-
-import sys
-sys.path.insert(0, "/Users/rodrigo.senra/r/projects/benchmark-python-wsgi/src")
 
 from settings import SPARQL_ENDPOINT
 from queries import GET_QUERY
@@ -36,9 +31,6 @@ def get():
     response, content = http.request(SPARQL_ENDPOINT + "?"+ params)
     return content
 
-#    http_client = HTTPClient.from_url(url, concurrency=1)
-#    response = http_client.get(url.request_uri)
-#    return response
 
 if __name__ == "__main__":
     http_server = WSGIServer(('', 8888), app)
