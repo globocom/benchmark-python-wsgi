36f089e7924971f20eef7c26548d17c3aac57fc1
icaromedeiros <icaro.medeiros@gmail.com>
2013-02-05 11:29:59 -0200

    sparqlwraper out of requirements. 1st version of load test using locust and tornado-virtuoso example

--- src/load_test/test_locust_load1.py
+++ src/load_test/test_locust_load1.py
@@ -3,23 +3,32 @@
 
 from locust import Locust, TaskSet, task
 
-class WebsiteTasks(TaskSet):
-    def on_start(self):
-        pass
-        #        self.client.post("/login", {
-        #            "username": "test_user",
-        #            "password": ""
-        #        })
+
+class VirtuosoTasks(TaskSet):
+
+#    def on_start(self):
+#        self.client.post("/login", {
+#            "username": "test_user",
+#            "password": ""
+#        })
 
     @task
-    def index(self):
+    def get(self):
         self.client.get("/")
 
-#    @task
-#    def about(self):
-#        self.client.get("/about/")
+    @task(50)
+    def post(self):
+        self.client.post("/")
 
-class WebsiteUser(Locust):
-    task_set = WebsiteTasks
+    @task
+    def delete(self):
+        self.client.delete("/")
+
+    @task
+    def get_after_delete(self):
+        self.client.get("/")
+
+class VirtuosoUser(Locust):
+    task_set = VirtuosoTasks
     min_wait = 5000
     max_wait = 15000
